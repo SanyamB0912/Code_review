c56bbb91d1990ee2a4d3fd91de8d646e275e85e1
Sanyam0912 <cb.en.u4cse22044@cb.students.amrita.edu>
2024-03-21 11:50:00 +0530

    made it run

---
+++ my-code-viewer/pages/code-pages.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import CodeViewer from '../components/CodeViewer';
+import '../styles/styles.css';
+
+const CodePage = () => {
+  const pythonCode = `def greet(name):
+    print("Hello, " + name)
+
+greet("World")`;
+
+  const javaCode = `public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}`;
+
+  return (
+    <div className="code-page">
+      <h1>Code Viewer</h1>
+      <CodeViewer code={pythonCode} language="python" />
+      <CodeViewer code={javaCode} language="java" />
+    </div>
+  );
+};
